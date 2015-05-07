1bd3de2a7d5f1d1410b9279c3a851072b7f1ee9e
Igor Matuszewski <Xanewok@gmail.com>
2015-05-07 18:40:43 +0000

    initially working screen with 2 windows, fix writing received messages

--- screen.rb
+++ screen.rb
@@ -8,25 +8,96 @@ PORT = 31337
 
 class Screen
   def initialize
-    @stdscr = initscr
-    @rows = getmaxx(
-    @cols
+    @server = TCPSocket.new("localhost", PORT)
+    @response = Thread.new do
+      loop do
+       msg = @server.gets.chomp
+       #send_msg
+       receive_msg(msg) 
+      end
+    end
 
-    @msgwin = newwin(3, 30, 15, 0)
-#    wborder(@msgwin, 0, 0, 0, 0, 0, 0, 0, 0)
-#    box(@msgwin, 0, 0)
-    @chatwin = newwin(7, 30, 0, 0)
-#    box(@chatwin, 0, 0)
+    @stdscr = initscr
+    @rows = getmaxy(stdscr)
+    @cols = getmaxx(stdscr)
+    @chatnum = 0
+    @msgsize = 3
+    @msg = []
+
+    @chatwin = newwin(@rows - @msgsize, @cols, 0, 0)
+    @msgwin = newwin(@msgsize, @cols, @rows - @msgsize, 0)
+
     cbreak
     noecho
 
-#    write "\n\n"
-#    wrefresh(@stdscr)
-#    clear
+    loop do
+      draw
+    end
   end
 
   def draw
+    y, x = getyx(stdscr)
+    # Resize our window
+    if (y != @cols) || (x != @rows)
+      @cols = y
+      @rows = x
+
+      wresize(@chatwin, y - @msgsize, x)
+      wresize(@msgwin, @msgsize, x)
+      mvwin(@msgwin, y - @msgsize, 0)
+
+      wclear(stdscr)
+      wclear(@chatwin)
+      wclear(@msgwin)
+
+      wmove(@msgwin, 1, 2)
+    end
+
+      box(@chatwin, 0, 0)
+      box(@msgwin, 0, 0)
+
+    wrefresh(@chatwin)
+    wrefresh(@msgwin)
+
+    handle_input(getch)
+  end
+
+  def handle_input(key)
+    @msg << key.chr
+    waddch(@msgwin, key)
+    send_msg if key.chr == "\n"
+  end
+
+  def send_msg
+    werase(@msgwin)
+    wmove(@msgwin, 1, 2)
+    wrefresh(@stdscr)
+    wrefresh(@msgwin)
     
+    tmp = @msg.join
+    @msg = []
+
+    @server.puts tmp
+#    return tmp
+  end
+
+  def receive_msg(msg)
+    return if msg == nil
+
+    # Not reached message limit in window
+    if (@chatnum < @rows - @msgsize - 2)
+      @chatnum += 1
+      # Get current cursor pos
+      cur_y, cur_x = getyx(@stdscr)
+      # Move cursor and write new msg
+      # FIXME:
+      mvprintw(@chatwin, @chatnum + 1, 1, msg)
+      # Go back to msg window
+      wmove(@msgwin, cur_y, cur_x)
+    else
+      # Move every goddamn message upwards
+      # then print the new one
+    end
   end
 
   def write(msg)
@@ -49,7 +120,6 @@ class Screen
       waddstr(@msgwin, key)
       if key == "\n"
         wclear(@msgwin)
-        refresh
         return msg.join
       end
 #      return msg.join if key == "\n"
