6a33986b8c1b7a20164d6831343fc60520799283
Bartek Gladecki <bgladecki@gmail.com>
2015-08-12 16:45:02 +0200

    Revert "no encryption nor ncurses. starting over"
    This reverts commit 0aa1170bc3f94fc1ce085268ec27102564dcbcad.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source 'https://rubygems.org'
 
 gem 'rsa'
-gem 'colorize'
+gem 'ffi-ncurses'
--- lib/oneop.rb
+++ lib/oneop.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 require 'socket'
 require 'rsa'
+require 'ffi-ncurses'
 
 Dir["./oneop/*.rb"].each {|file| require file }
 
@@ -19,4 +20,41 @@ else
 end
 
 Server.new(CLIENT == true, PORT) if SERVER
-Client.new(DebugConsole.new) if CLIENT
+
+if CLIENT
+  begin
+    server = TCPSocket.new(HOST, PORT)
+    return unless server
+
+    screen = Screen.new
+    screen.receive "connected to: " + server.addr[2] + ":" + server.addr[1].to_s
+
+    server_listener = Thread.new do
+      loop do
+        msg = server.gets.chomp
+        screen.receive msg
+      end
+    end
+
+    # Main loop
+    # Outside Screen class so we can use initialize Screen correctly and block on getch
+    loop do
+      c = screen.getch
+      exit if c == 3 #FIXME: interrupt, silly ncurses swalloing every control char
+
+      if c.between?(0, 255)
+        if (c.chr != "\n")
+          screen.append(c)
+        else
+          server.puts screen.get_msg
+          screen.flush_msg
+        end
+
+        #    screen.draw
+      end
+    end
+
+  ensure
+    screen.endwin if screen
+  end
+end
